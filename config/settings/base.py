2780dc09b6357699c05b22d0448b46d18acaf8f0
Jack Morgan <jackmorgannz@gmail.com>
2018-05-07 09:24:27 +1200

    Update logging configuration

--- config/settings/base.py
+++ config/settings/base.py
@@ -195,39 +195,6 @@ PASSWORD_HASHERS = [
     "django.contrib.auth.hashers.BCryptPasswordHasher",
 ]
 
-LOGGING = {
-    "version": 1,
-    "disable_existing_loggers": False,
-    "formatters": {
-        "verbose": {
-            "format": ("%(asctime)s [%(process)d] [%(levelname)s] " +
-                       "pathname=%(pathname)s lineno=%(lineno)s " +
-                       "funcname=%(funcName)s %(message)s"),
-            "datefmt": "%Y-%m-%d %H:%M:%S"
-        },
-        "simple": {
-            "format": "%(levelname)s %(message)s"
-        }
-    },
-    "handlers": {
-        "null": {
-            "level": "DEBUG",
-            "class": "logging.NullHandler",
-        },
-        "console": {
-            "level": "DEBUG",
-            "class": "logging.StreamHandler",
-            "formatter": "verbose"
-        }
-    },
-    "loggers": {
-        "testlogger": {
-            "handlers": ["console"],
-            "level": "INFO",
-        }
-    }
-}
-
 # AUTH
 # ------------------------------------------------------------------------------
 AUTHENTICATION_BACKENDS = (
