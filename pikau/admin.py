412ad298571b5e1fb13eb773764979f11f3d68e4
Jack Morgan <jackmorgannz@gmail.com>
2018-05-09 14:29:58 +1200

    Allow full control of pikau for admin

--- pikau/admin.py
+++ pikau/admin.py
@@ -6,34 +6,6 @@ from .models import PikauCourse
 
 class PikauCourseAdmin(admin.ModelAdmin):
     list_display = ("name", "language", "topic", "level")
-    fieldsets = [
-        (
-            None,
-            {"fields": [
-                "name",
-                "slug",
-            ]}
-        ),
-        (
-            "Metadata",
-            {"fields": [
-                "language",
-                "topic",
-                "level",
-                "progress_outcomes",
-                "tags",
-            ]}
-        ),
-        (
-            "Development Information",
-            {"fields": [
-                "development_folder",
-                "status",
-                "status_updated",
-            ]}
-        ),
-    ]
     filter_horizontal = ("tags", "progress_outcomes")
-    readonly_fields = ("status_updated", )
 
 admin.site.register(PikauCourse, PikauCourseAdmin)
