4a74612919b4de861b529384b413c336ec36355f
Jack Morgan <jackmorgannz@gmail.com>
2018-05-18 14:06:05 +1200

    (WIP) Add admin action for updating course status

--- pikau/admin.py
+++ pikau/admin.py
@@ -1,6 +1,7 @@
 """Admin configuration for the pikau application."""
 
 from django.contrib import admin
+from django.shortcuts import render
 from pikau.models import (
     Level,
     Milestone,
@@ -8,6 +9,7 @@ from pikau.models import (
     ProgressOutcome,
     Tag,
     Topic,
+    STATUS_CHOICES,
 )
 
 
@@ -16,6 +18,27 @@ class PikauCourseAdmin(admin.ModelAdmin):
 
     list_display = ("name", "language", "status", "readiness_level", "topic", "level")
     filter_horizontal = ("tags", "progress_outcomes", "prerequisites")
+    actions = ["update_status"]
+
+    def update_status(self, request, queryset):
+        return render(request, "admin/status_intermediate.html", context={})
+        context["status_stages"] = STATUS_CHOICES
+
+    def update_status(self, request, queryset):
+        # All requests here will actually be of type POST
+        # so we will need to check for our special key 'apply'
+        # rather than the actual request type.
+        print(request.POST, queryset)
+        if "new_status" in request.POST:
+            queryset.update(status=new_status)
+            self.message_user(
+                request,
+                "Changed status on {} pīkau".format(queryset.count())
+            )
+            return HttpResponseRedirect(request.get_full_path())
+        return render(request, "admin/status_intermediate.html", context={"status_stages": STATUS_CHOICES})
+
+    update_status.short_description = "Update course status"
 
 
 admin.site.register(PikauCourse, PikauCourseAdmin)
