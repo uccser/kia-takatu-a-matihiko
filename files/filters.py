16a1713b49009ee81eb01411405cb14c8150ec2b
Jack Morgan <jackmorgannz@gmail.com>
2018-05-20 20:26:34 +1200

    Docstring fixes

--- files/filters.py
+++ files/filters.py
@@ -1,9 +1,13 @@
+"""Filters for the files application."""
+
 import django_filters
 from files.models import File
 
 
 class FileFilter(django_filters.FilterSet):
+    """File filter for the files table."""
 
     class Meta:
+        """Meta attributes for FileFilter class."""
         model = File
         fields = ["licence"]
--- files/tables.py
+++ files/tables.py
@@ -1,3 +1,5 @@
+"""Tables for the files application."""
+
 import django_tables2 as tables
 from files.models import (
     File,
@@ -5,7 +7,9 @@ from files.models import (
 
 
 class FileTable(tables.Table):
+    """Table to display all files."""
 
     class Meta:
+        """Meta attributes for FileTable class."""
         model = File
         fields = ("filename", "licence")
