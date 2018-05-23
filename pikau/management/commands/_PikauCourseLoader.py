fc533f3676a711d7569b09929269fb6e4bad6724
Jack Morgan <jackmorgannz@gmail.com>
2018-05-23 22:32:02 +1200

    Add "Getting the most out of your time" pikau

--- pikau/management/commands/_PikauCourseLoader.py
+++ pikau/management/commands/_PikauCourseLoader.py
@@ -28,30 +28,38 @@ class PikauCourseLoader(BaseLoader):
         for pikau_course_slug in pikau_courses.get("courses", list()):
             self.base_path = os.path.join(self.original_base_path, "pikau-courses", pikau_course_slug)
             pikau_course_metadata = self.load_yaml_file("metadata.yaml")
-            pikau_course_overview = self.convert_md_file(
-                pikau_course_metadata["overview"],
-                CONFIG_FILE,
-                heading_required=False,
-                remove_title=False,
-            ).html_string
-            pikau_course_study_plan = self.convert_md_file(
-                pikau_course_metadata["study-plan"],
-                CONFIG_FILE,
-                heading_required=False,
-                remove_title=False,
-            ).html_string
-            pikau_course_assessment_description = self.convert_md_file(
-                pikau_course_metadata["assessment-description"],
-                CONFIG_FILE,
-                heading_required=False,
-                remove_title=False,
-            ).html_string
-            pikau_course_assessment_items = self.convert_md_file(
-                pikau_course_metadata["assessment-items"],
-                CONFIG_FILE,
-                heading_required=False,
-                remove_title=False,
-            ).html_string
+            pikau_course_overview = pikau_course_metadata.get("overview", "")
+            if pikau_course_overview:
+                pikau_course_overview = self.convert_md_file(
+                    pikau_course_metadata["overview"],
+                    CONFIG_FILE,
+                    heading_required=False,
+                    remove_title=False,
+                ).html_string
+            pikau_course_study_plan = pikau_course_metadata.get("study-plan", "")
+            if pikau_course_study_plan:
+                pikau_course_study_plan = self.convert_md_file(
+                    pikau_course_metadata["study-plan"],
+                    CONFIG_FILE,
+                    heading_required=False,
+                    remove_title=False,
+                ).html_string
+            pikau_course_assessment_description = pikau_course_metadata.get("assessment-description", "")
+            if pikau_course_assessment_description:
+                pikau_course_assessment_description = self.convert_md_file(
+                    pikau_course_metadata["assessment-description"],
+                    CONFIG_FILE,
+                    heading_required=False,
+                    remove_title=False,
+                ).html_string
+            pikau_course_assessment_items = pikau_course_metadata.get("assessment-items", "")
+            if pikau_course_assessment_items:
+                pikau_course_assessment_items = self.convert_md_file(
+                    pikau_course_metadata["assessment-items"],
+                    CONFIG_FILE,
+                    heading_required=False,
+                    remove_title=False,
+                ).html_string
 
             defaults = {
                 "name": pikau_course_metadata["name"],
@@ -59,7 +67,7 @@ class PikauCourseLoader(BaseLoader):
                 "language": pikau_course_metadata["language"],
                 "topic": Topic.objects.get(slug=pikau_course_metadata["topic"]),
                 "level": Level.objects.get(slug=pikau_course_metadata["level"]),
-                "trailer_video": pikau_course_metadata["trailer-video"],
+                "trailer_video": pikau_course_metadata.get("trailer-video", ""),
                 "cover_photo": pikau_course_metadata.get("cover-photo", "images/pikau-course-cover.png"),
                 "overview": pikau_course_overview,
                 "readiness_level": pikau_course_metadata.get("readiness-level"),
