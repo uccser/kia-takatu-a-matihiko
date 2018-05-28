f4cedd9e9d5e1666340210f58f38dbfedaf17382
Courtney Bracefield <courtneybracefield86@gmail.com>
2018-05-28 18:43:48 +1200

    Add test for pikau documentation view.

---
+++ tests/pikau/views/test_documentation_view.py
@@ -0,0 +1,15 @@
+from tests.BaseTestWithDB import BaseTestWithDB
+from django.urls import reverse
+from http import HTTPStatus
+
+
+class DocumentationViewTest(BaseTestWithDB):
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.language = "en"
+
+    def test_index_view(self):
+        response = self.client.get(reverse("pikau:docs"))
+        self.assertEqual(HTTPStatus.OK, response.status_code)
+        self.assertContains(response, "Pīkau Documentation")
