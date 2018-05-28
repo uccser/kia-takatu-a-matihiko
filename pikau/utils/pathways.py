91eee5015bda313bcee279a69e8521daca40e9fe
Jack Morgan <jackmorgannz@gmail.com>
2018-05-28 12:28:28 +1200

    Show pikau on pathway when required

--- pikau/utils/pathways.py
+++ pikau/utils/pathways.py
@@ -1,6 +1,8 @@
 """Module for creating pikau course pathways."""
 
 from pikau.models import PikauCourse, READINESS_LEVELS
+from django.db.models import Q, Count
+
 
 GRAPH_TEMPLATE = (
     "digraph {{"
@@ -10,7 +12,7 @@ GRAPH_TEMPLATE = (
     "{edges}"
     "}}"
 )
-NODE_TEMPLATE = "{id} [label=\"{name}\", href=\"{url}\", color=\"{color}\", penwidth=4.0];"
+NODE_TEMPLATE = "{id} [label=\"{name}\", href=\"{url}\", color=\"{color}\", penwidth={penwidth}.0];"
 EDGE_TEMPLATE = "{start_id} -> {end_id};"
 
 
@@ -20,16 +22,25 @@ def create_pathways_notation():
     Returns:
         String of graph notation.
     """
-    all_pikau = PikauCourse.objects.exclude(readiness_level__isnull=True)
+    all_pikau = PikauCourse.objects.annotate(Count("postrequisites")).filter(
+        Q(readiness_level__isnull=False) | Q(postrequisites__count__gt=0)
+    )
 
     # Create nodes
     nodes = []
     for pikau in all_pikau:
+        if pikau.readiness_level in READINESS_LEVELS:
+            color = READINESS_LEVELS[pikau.readiness_level]["color"]
+            pen_width = 4
+        else:
+            color = "#444444"
+            pen_width = 2
         node = NODE_TEMPLATE.format(
             id=pikau.id,
             name=pikau.__str__(),
             url=pikau.get_absolute_url(),
-            color=READINESS_LEVELS[pikau.readiness_level]["color"]
+            color=color,
+            penwidth=pen_width,
         )
         nodes.append(node)
 
