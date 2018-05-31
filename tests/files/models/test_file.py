89b55c0c3694b135e95a06dd270de3c458368099
Courtney Bracefield <courtneybracefield86@gmail.com>
2018-06-01 11:02:22 +1200

    Add more tests to test file model.

--- tests/files/models/test_file.py
+++ tests/files/models/test_file.py
@@ -1,6 +1,8 @@
 from tests.BaseTestWithDB import BaseTestWithDB
 from tests.files.FileTestDataGenerator import FileTestDataGenerator
 
+from files.models import File
+from django.db import IntegrityError
 
 class FileModelTest(BaseTestWithDB):
 
@@ -13,4 +15,28 @@ class FileModelTest(BaseTestWithDB):
         self.assertEqual(
             obj.__str__(),
             "File 1"
+        )
+
+    def test_file_model_one_file(self):
+        file = self.test_data.create_file(1)
+        query_result = File.objects.get(slug="file-1")
+        self.assertEqual(query_result, file)
+
+    def test_file_model_two_files(self):
+        file_1 = self.test_data.create_file(1)
+        file_2 = self.test_data.create_file(2)
+        self.assertQuerysetEqual(
+            File.objects.all(),
+            [
+                "File: file-1",
+                "File: file-2"
+            ],
+            ordered=False
+        )
+
+    def test_file_model_uniqueness(self):
+        file = self.test_data.create_file(1)
+        self.assertRaises(
+            IntegrityError, 
+            lambda: self.test_data.create_file(1)
         )
