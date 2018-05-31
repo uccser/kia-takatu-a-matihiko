f127267d3bc6386ceedfe460a30d395eaf76bfac
Courtney Bracefield <courtneybracefield86@gmail.com>
2018-06-01 11:41:41 +1200

    Add TODO statement for progress outcome view tests.

---
+++ tests/pikau/views/test_progress_outcome_view.py
@@ -0,0 +1,37 @@
+from tests.BaseTestWithDB import BaseTestWithDB
+from tests.pikau.PikauTestDataGenerator import PikauTestDataGenerator
+from django.urls import reverse
+
+
+class ProgressOutcomeViewTest(BaseTestWithDB):
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.language = "en"
+        self.test_data = PikauTestDataGenerator()
+
+    def test_pikau_progress_outcome_view_with_valid_slug(self):
+        progress_outcome = self.test_data.create_progress_outcome(1)
+
+        kwargs = {
+            "slug": "progress-outcome-1",
+        }
+        url = reverse("pikau:progress_outcome", kwargs=kwargs)
+        response = self.client.get(url)
+        self.assertEqual(200, response.status_code)
+        self.assertEqual(
+            url, 
+            "/pikau/progress-outcomes/progress-outcome-1/"
+        )
+
+    def test_pikau_progress_outcome_view_with_invalid_slug(self):
+        progress_outcome = self.test_data.create_progress_outcome(1)
+
+        kwargs = {
+            "slug": "progress-outcome-5",
+        }
+        url = reverse("pikau:progress_outcome", kwargs=kwargs)
+        response = self.client.get(url)
+        self.assertEqual(404, response.status_code)
+
+    # TODO: Add tests for heatmap.
