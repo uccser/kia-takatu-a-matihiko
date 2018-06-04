7f27029175bc539bbc264318b6e65bb98579cdc4
Courtney Bracefield <courtneybracefield86@gmail.com>
2018-06-05 11:32:06 +1200

    Make test_pikau_course.py adhere to PEP8 styling.

--- tests/pikau/models/test_pikau_course.py
+++ tests/pikau/models/test_pikau_course.py
@@ -19,59 +19,58 @@ class PikauCourseModelTest(BaseTestWithDB):
         )
 
     def test_pikau_course_is_overdue_past_milestone_date(self):
-    	pikau_course = self.test_data.create_pikau_course(1)
-    	milestone_date = datetime.now() - dt.timedelta(days=30)
-    	pikau_course.milestone = self.test_data.create_milestone(1, milestone_date.date())
-    	self.assertEqual(
+        pikau_course = self.test_data.create_pikau_course(1)
+        milestone_date = datetime.now() - dt.timedelta(days=30)
+        pikau_course.milestone = self.test_data.create_milestone(1, milestone_date.date())
+        self.assertEqual(
             pikau_course.is_overdue_milestone,
             True
         )
 
     def test_pikau_course_is_overdue_future_milestone_date(self):
-    	pikau_course = self.test_data.create_pikau_course(1)
-    	milestone_date = datetime.now() + dt.timedelta(days=30)
-    	pikau_course.milestone = self.test_data.create_milestone(1, milestone_date.date())
-    	self.assertEqual(
+        pikau_course = self.test_data.create_pikau_course(1)
+        milestone_date = datetime.now() + dt.timedelta(days=30)
+        pikau_course.milestone = self.test_data.create_milestone(1, milestone_date.date())
+        self.assertEqual(
             pikau_course.is_overdue_milestone,
             False
         )
 
     def test_pikau_course_is_overdue_current_milestone_date(self):
-    	pikau_course = self.test_data.create_pikau_course(1)
-    	milestone_date = datetime.now()
-    	pikau_course.milestone = self.test_data.create_milestone(1, milestone_date.date())
-    	self.assertEqual(
+        pikau_course = self.test_data.create_pikau_course(1)
+        milestone_date = datetime.now()
+        pikau_course.milestone = self.test_data.create_milestone(1, milestone_date.date())
+        self.assertEqual(
             pikau_course.is_overdue_milestone,
             False
         )
 
     def test_pikau_course_is_overdue_course_complete_past_milestone_date(self):
-    	pikau_course = self.test_data.create_pikau_course(1)
-    	milestone_date = datetime.now() - dt.timedelta(days=30)
-    	pikau_course.milestone = self.test_data.create_milestone(1, milestone_date.date())
-    	pikau_course.status = 7
-    	self.assertEqual(
+        pikau_course = self.test_data.create_pikau_course(1)
+        milestone_date = datetime.now() - dt.timedelta(days=30)
+        pikau_course.milestone = self.test_data.create_milestone(1, milestone_date.date())
+        pikau_course.status = 7
+        self.assertEqual(
             pikau_course.is_overdue_milestone,
             False
         )
 
     def test_pikau_course_is_overdue_course_complete_future_milestone_date(self):
-    	pikau_course = self.test_data.create_pikau_course(1)
-    	milestone_date = datetime.now() + dt.timedelta(days=30)
-    	pikau_course.milestone = self.test_data.create_milestone(1, milestone_date.date())
-    	pikau_course.status = 7
-    	self.assertEqual(
+        pikau_course = self.test_data.create_pikau_course(1)
+        milestone_date = datetime.now() + dt.timedelta(days=30)
+        pikau_course.milestone = self.test_data.create_milestone(1, milestone_date.date())
+        pikau_course.status = 7
+        self.assertEqual(
             pikau_course.is_overdue_milestone,
             False
         )
 
     def test_pikau_course_is_overdue_course_complete_current_milestone_date(self):
-    	pikau_course = self.test_data.create_pikau_course(1)
-    	milestone_date = datetime.now()
-    	pikau_course.milestone = self.test_data.create_milestone(1, milestone_date.date())
-    	pikau_course.status = 7
-    	self.assertEqual(
+        pikau_course = self.test_data.create_pikau_course(1)
+        milestone_date = datetime.now()
+        pikau_course.milestone = self.test_data.create_milestone(1, milestone_date.date())
+        pikau_course.status = 7
+        self.assertEqual(
             pikau_course.is_overdue_milestone,
             False
         )
-
