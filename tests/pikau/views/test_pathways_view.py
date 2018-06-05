eaf01d1084e2ddb1f2d0db9e429e4d52dfbe1685
Courtney Bracefield <courtneybracefield86@gmail.com>
2018-06-05 12:05:15 +1200

    Make test_pathways_view.py adhere to PEP8 styling.

--- tests/pikau/views/test_pathways_view.py
+++ tests/pikau/views/test_pathways_view.py
@@ -16,7 +16,7 @@ class PathwaysViewTest(BaseTestWithDB):
 
     def test_pathways_view_context_readiness_levels(self):
         response = self.client.get(reverse("pikau:pathways"))
-        self.assertEqual(200, response.status_code)   
+        self.assertEqual(200, response.status_code)
         self.assertEqual(response.context["readiness_levels"], READINESS_LEVELS)
 
     # TODO: test pathways notation - utils test.
